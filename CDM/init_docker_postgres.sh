8e5a96fc77b0268f7c798350760b0c7e92d06cdd
Ali <contact@alibellamine.me>
2022-07-05 14:16:50 +0200

    Adding vocabularies

--- CDM/init_docker_postgres.sh
+++ CDM/init_docker_postgres.sh
@@ -5,16 +5,12 @@ psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE SCHEMA IF NOT EXISTS $OMOP_SCH
 sed "s/@cdmDatabaseSchema/$OMOP_SCHEMA_NAME/g" /data/DDL/OMOPCDM_postgresql_5.4_ddl.sql | psql -U $POSTGRES_USER -d $POSTGRES_DB 
 sed "s/@cdmDatabaseSchema/$OMOP_SCHEMA_NAME/g" /data/DDL/OMOPCDM_postgresql_5.4_indices.sql | psql -U $POSTGRES_USER -d $POSTGRES_DB 
 sed "s/@cdmDatabaseSchema/$OMOP_SCHEMA_NAME/g" /data/DDL/OMOPCDM_postgresql_5.4_primary_keys.sql | psql -U $POSTGRES_USER -d $POSTGRES_DB 
+sed "s/@cdmDatabaseSchema/$OMOP_SCHEMA_NAME/g" /data/DDL/OMOPCDM_postgresql_5.4_constraints.sql | psql -U $POSTGRES_USER -d $POSTGRES_DB 
 
 # Populate vocabularies
-fileNames=("CONCEPT" "VOCABULARY" "DOMAIN" "CONCEPT_CLASS" "CONCEPT_ANCESTOR" "CONCEPT_RELATIONSHIP" "CONCEPT_SYNONYM" "DRUG_STRENGTH" "RELATIONSHIP")
-
-for fileName in ${fileNames[@]}; do
-    echo "/data/vocabularies/$fileName.csv"
+fileNames=("CONCEPT", "CONCEPT_ANCESTOR", "CONCEPT_CLASS", "CONCEPT_RELATIONSHIP", "CONCEPT_SYNONYM", "DOMAIN", "DRUG_STRENGTH", "RELATIONSHIP", "VOCABULARY", "test")
+for fileName in ${fileName[@]}; do
     if test -f "/data/vocabularies/$fileName.csv"; then
-        psql -U $POSTGRES_USER -d $POSTGRES_DB -c "\COPY $OMOP_SCHEMA_NAME.$fileName FROM '/data/vocabularies/$fileName.csv' DELIMITER E'\t' CSV HEADER QUOTE E'\b';"
+        echo $fileName
     fi
 done
-
-# Populate constraints
-sed "s/@cdmDatabaseSchema/$OMOP_SCHEMA_NAME/g" /data/DDL/OMOPCDM_postgresql_5.4_constraints.sql | psql -U $POSTGRES_USER -d $POSTGRES_DB 
